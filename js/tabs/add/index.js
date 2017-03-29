643af20098c55950f053686dba8294ed586a6f13
Cesar Cruz <cesarcruz91@gmail.com>
2017-03-28 23:15:44 -0400

    Check camera permissions

--- js/tabs/add/index.js
+++ js/tabs/add/index.js
@@ -9,16 +9,29 @@ import {
   TextInput
 } from 'react-native'
 
-import { ImagePicker } from 'expo'
+import { ImagePicker, Permissions, Location } from 'expo'
 
 export default class Add extends Component {
-  componentDidMount () {
-    console.log('Mounted')
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      location: null,
+      locationCaptured: false,
+      category: '',
+      sketchyness: '',
+      availability: ''
+    }
   }
 
   _launchCamera () {
-    console.log('pic')
-    ImagePicker.launchCameraAsync()
+    Permissions.getAsync(Permissions.CAMERA)
+      .then(result => {
+        if (result.status === 'granted') {
+          return ImagePicker.launchCameraAsync()
+        }
+      })
       .then(data => {
         console.log(data)
       })
@@ -27,9 +40,44 @@ export default class Add extends Component {
       })
   }
 
+  _captureLocation () {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(result => {
+        console.log(result)
+        if (result.status === 'granted') {
+          return Location.getCurrentPositionAsync()
+        }
+        return Promise.resolve()
+      })
+      .then(coords => {
+        console.log(coords)
+        if (coords) {
+          this.setState({
+          location: {
+            lat: coords.latitude,
+            lon: coords.longitude
+          },
+          locationCaptured: true
+        })
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  _createSpot () {
+    
+  }
+
   render () {
     return (
       <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View style={styles.header}>
+            <Image source={require('../../../assets/icons/ic_elspotpink.png')} />
+          </View>
+        </View>
         <View style={styles.addwrapper}>
           <View style={styles.add}>
             <TouchableWithoutFeedback onPress={this._launchCamera}>
@@ -38,12 +86,22 @@ export default class Add extends Component {
           </View>
         </View>
 
-        <View style={styles.inpwrapper}>
-        <View style={styles.txtwrapper}>
-          <Text style={styles.txt}>Location:</Text>
-        </View>
-          <View style={styles.inp}>
-            <TextInput/>
+          <View style={styles.txtwrapper}>
+            <Text style={styles.txt}>Location:</Text>
+          </View>
+          <View style={styles.field}>
+            {
+              this.state.locationCaptured ?
+              <View style={styles.btn}>
+                <Text textAlign='center' style={{color: 'green'}}>Captured!</Text>
+              </View> :
+              <TouchableWithoutFeedback onPress={this._captureLocation.bind(this)}>
+                <View style={styles.btn}>
+                  <Text textAlign='center' style={{color: '#B0F6E6'}}>Capture Location</Text>
+                </View>
+              </TouchableWithoutFeedback>
+            }
+            
           </View>
 
           <View style={styles.txtwrapper}>
@@ -56,8 +114,13 @@ export default class Add extends Component {
           <View style={styles.txtwrapper}>
             <Text style={styles.txt}>Sketchyness:</Text>
           </View>
-          <View style={styles.inp}>
-            <TextInput/>
+          <View style={{ borderBottomWidth: 1, borderBottomColor: 'white'}}>
+            <TextInput
+              style={styles.input}
+              autoCorrect={false}
+              onChangeText={(text) => this.setState({ category: text })}
+              value={this.state.category}
+            />
           </View>
 
           <View style={styles.txtwrapper}>
@@ -66,7 +129,13 @@ export default class Add extends Component {
           <View style={styles.inp}>
             <TextInput/>
           </View>
-        </View>
+          <TouchableWithoutFeedback>
+            <View>
+              <Text>
+                Create Spot
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -78,6 +147,16 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     flex: 1,
     backgroundColor: '#252839'
+  },
+  headerContainer: {
+    height: 55,
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EDF5FA',
+    paddingTop: 10
   },
   addwrapper: {
     flex: 2,
@@ -102,8 +181,20 @@ const styles = StyleSheet.create({
   inpwrapper: {
     flex: 3
   },
-  inp: {
-    flex: 2,
-    justifyContent: 'space-around'
+  input: {
+    height: 25,
+    width: Dimensions.get('window').width - 15,
+    color: 'white'
+  },
+  field: {
+    width: Dimensions.get('window').width,
+    height: 50,
+    padding: 5
+  },
+  btn: {
+    backgroundColor: '#E0106D',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
